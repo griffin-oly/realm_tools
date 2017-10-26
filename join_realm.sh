ffc01126d2def612b7dd7b135d78ad09ace4b6fe
Griffin <griffin.goodman@olympus.com>
2017-10-26 16:17:59 -0400

    added splunk vars.

--- join_realm.sh
+++ join_realm.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible-playbook -i inventory -v join_realm.yml -u ibduser --ask-pass --ask-sudo
+ansible-playbook -i inventory -v join_realm.yml -u ibsuser --ask-pass --ask-become-pass
--- sys_setup.sh
+++ sys_setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible-playbook -i inventory -v system_setup.yml -u adm_goodmg --ask-pass
+ansible-playbook -i inventory -v system_setup.yml -u ibsuser --ask-pass --ask-become-pass

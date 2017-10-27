f8844dad69d20faa7c73e66b5fe7179129f63d6d
Griffin <griffin.goodman@olympus.com>
2017-10-27 08:51:47 -0400

    changed host key checking

--- join_realm.sh
+++ join_realm.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible-playbook -i inventory -v join_realm.yml -u ibsuser --ask-pass --ask-become-pass
+ansible-playbook -i inventory -v -e 'host_key_checking=False' join_realm.yml -u ibduser --ask-pass --ask-become-pass
